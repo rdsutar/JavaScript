a5cb9fb60c32cb18bdaff8bcc5950d6ffb8530fc
Rohan Sutar <58723613+rdsutar@users.noreply.github.com>
2023-09-15 18:49:12 +0000

    Datatypes

---
+++ 01_Basics/02_Datatypes.js
@@ -0,0 +1,27 @@
+"use strict"; // Treate all JS code as a newer version
+
+// alert(3+3); // We are using Node JS, not browser
+
+console.log(3+
+    3); // Code readablity should be high
+
+console.log("Rohan");
+
+let name = "Rohan";
+let age = 30;
+let isLoggedIn = true;
+
+
+// number => 2 to power 53
+// bigint => 
+// string => ""
+// boolean => true/false
+// null => standalone value
+// undefined =>
+// symbol => unique
+
+// object
+
+console.log(typeof undefined); // undefined
+console.log(typeof null); // object
+

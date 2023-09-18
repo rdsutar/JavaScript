5c31d232746a5e746ba05e288071c649addf89b2
Rohan Sutar <58723613+rdsutar@users.noreply.github.com>
2023-09-18 17:34:01 +0000

    Operations

---
+++ 01_Basics/03_ConversionOperations.js
@@ -0,0 +1,18 @@
+let value = 3;
+let negValue = -value;
+
+// console.log(negValue);
+
+// console.log(2+2);
+// console.log(2-2);
+// console.log(2*2);
+// console.log(2/2);
+// console.log(2**3);
+// console.log(2%3);
+
+let str1 = "Hello";
+let str2 = "Rohan";
+
+let str3 = str1 + str2;
+console.log(str3);
+
